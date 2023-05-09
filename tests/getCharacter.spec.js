5168c0ff4555ffdc8e302eb2ae756cf28c5c3378
Mariana Ramiro <marianaramiroof@gmail.com>
2023-05-09 12:25:46 -0300

    requisito 9

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -44,13 +44,18 @@ Retorno:
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // 1. Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    // 2. Teste se a função retorna o objeto correto para o parâmetro 'Arya',
-    // 3. Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
-    // 4. Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
-    // 5. Teste se o parâmetro não é Case Sensitive, ou seja, independente de conter letras maiúsculas ou minúsculas retorna o mesmo objeto relativo a ele.
-    // 6. Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+    // fail('Teste vazio!');
+    expect(getCharacter(' ')).toBeUndefined();
+    expect(getCharacter('Arya')).toHaveReturned({name: 'Arya Stark', class: 'Rogue', phrases: [ 'Not today', 'A girl has no name.' ]});
+    expect(getCharacter('Brienne')).toHaveReturned({name: 'Brienne Tarth', class: 'Knight', phrases: [ 'Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.']});
+    expect(getCharacter('Melissandre')).toHaveReturned({name: 'Melissandre ', class: 'Necromancer', phrases: [ 'Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.' ]});
+    expect(getCharacter('Maria')).toBeUndefined();
   });
 });
+ // ESCREVA SEUS TESTES ABAIXO:
+  // 1. Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+  // 2. Teste se a função retorna o objeto correto para o parâmetro 'Arya',
+  // 3. Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
+  // 4. Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
+  // 5. Teste se o parâmetro não é Case Sensitive, ou seja, independente de conter letras maiúsculas ou minúsculas retorna o mesmo objeto relativo a ele.
+  // 6. Teste se ao passar um nome que não está na tabela, a função retorna undefined.
